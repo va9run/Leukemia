d2c75c090ba29806aa206ad578832aaa96e3b762
varun yadav <varun@keyninja.io>
2023-03-20 21:38:53 +1100

    Basic import

---
+++ Task_1/A1_path.py
@@ -0,0 +1,2 @@
+trainingDataHEM = 'C:/Users/v4run/OneDrive/Desktop/Datasets/C-NMC_Leukemia/training_data/fold_1/hem'
+trainingDataALL = 'C:/Users/v4run/OneDrive/Desktop/Datasets/C-NMC_Leukemia/training_data/fold_1/all'
---
+++ Task_1/A_Libraries.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+from PIL import Image
+import os
