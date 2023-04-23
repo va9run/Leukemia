c422ae7a56ce4af378702c9461b0c2f833867c0a
varun yadav <varun@keyninja.io>
2023-04-23 20:34:04 +1000

    image cropping

--- Try_5/imageCropping.py
+++ Try_5/imageCropping.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.image import resize
+import cv2
 
 
 def crop_img(images, display=False):
@@ -14,6 +15,9 @@ def crop_img(images, display=False):
     cropped_image = images[top_left[0]:bottom_right[0],top_left[1]:bottom_right[1]]
     
     # Convert cropped_image numpy array to TensorFlow tensor
-    cropped_image_tensor = tf.convert_to_tensor(cropped_image, dtype=tf.float32)
-    resized_image = resize(cropped_image_tensor,[256,256])
-    return resized_image
+    resized_image = cv2.resize(cropped_image, (256, 256))
+
+    # Convert resized_image numpy array to TensorFlow tensor
+    resized_image_tensor = tf.convert_to_tensor(resized_image, dtype=tf.float32)
+    
+    return resized_image_tensor
