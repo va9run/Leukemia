27eb04ac09e1803af368331e8ddd040a5360fe49
varun yadav <varun@keyninja.io>
2023-04-23 21:07:22 +1000

    Model 5 shit

--- Try_5/imageCropping.py
+++ Try_5/imageCropping.py
@@ -14,10 +14,8 @@ def crop_img(images, display=False):
 
     cropped_image = images[top_left[0]:bottom_right[0],top_left[1]:bottom_right[1]]
     
-    # Convert cropped_image numpy array to TensorFlow tensor
+    # Resize cropped images
     resized_image = cv2.resize(cropped_image, (256, 256))
 
-    # Convert resized_image numpy array to TensorFlow tensor
-    resized_image_tensor = tf.convert_to_tensor(resized_image, dtype=tf.float32)
-    
-    return resized_image_tensor
+    resized_image = resized_image.astype('float32')/255.0
+    return resized_image
